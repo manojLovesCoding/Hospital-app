6c548e91d3752ede9d5c9e27ccd422a37cd5590d
manojLovesCoding <mkum9417@gmail.com>
2024-09-08 19:25:19 +0530

    Built TopDoctors Component and created a file named AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,10 +1,6 @@
 import { createContext } from "react";
-<<<<<<< HEAD
-import { doctors } from "../assets/assets";
 
-=======
 import { doctors } from '../assets/assets'
->>>>>>> 3549b662091fc53978f0334157c9d9f5030423a1
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
@@ -13,16 +9,10 @@ const AppContextProvider = (props) => {
     }
 
     return (
-<<<<<<< HEAD
+
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
-
-=======
-        <AppContextProvider value={value}>
-            {props.children}
-        </AppContextProvider>
->>>>>>> 3549b662091fc53978f0334157c9d9f5030423a1
     )
 }
 
