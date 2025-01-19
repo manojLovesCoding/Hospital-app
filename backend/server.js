e1a63c2edec83644311579c07bb115f5df947c16
manojLovesCoding <mkum9417@gmail.com>
2025-01-19 20:15:48 +0530

    successfully created api for adding doctor.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 //app config
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //api endpoints
-app.get('/', (req, res) => {
-    res.send('API WORKING great')
-})
+app.use("/api/admin", adminRouter); //localhost:4000/api/admin/add-doctor
 
-app.listen(port, () => console.log("Server Started", port))
+app.get("/", (req, res) => {
+  res.send("API WORKING great");
+});
+
+app.listen(port, () => console.log("Server Started", port));
