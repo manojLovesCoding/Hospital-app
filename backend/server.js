7850fbf99634a541678e4195ab583ca6549a5520
manojLovesCoding <mkum9417@gmail.com>
2025-01-31 15:35:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-
+import adminRouter from "./routes/adminRoute.js";
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(cors());
 
 //api endpoints
+app.use("/api/admin", adminRouter); //localhost:4000/api/admin/add-doctor
+
 app.get("/", (req, res) => {
   res.send("API WORKING great");
 });
