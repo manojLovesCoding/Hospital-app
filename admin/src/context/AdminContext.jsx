7d2ef1dc76fd56b1e0b0d508fe451631743a16ec
manojLovesCoding <mkum9417@gmail.com>
2025-02-05 18:28:27 +0530

    shown doctor-card on Doctor-List-Page and created changeAvailability function sucessfully

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,8 +7,10 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
+
   const [doctors, setDoctors] = useState([]);
   const backendUrl = import.meta.env.VITE_BACKENDURL;
+
   const getAllDoctors = async () => {
     try {
       const { data } = await axios.post(
@@ -26,12 +28,32 @@ const AdminContextProvider = (props) => {
       toast.error(error.message);
     }
   };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/change-availability",
+        { docId },
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctors();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     aToken,
     setAToken,
     backendUrl,
     doctors,
-    getAllDoctors
+    getAllDoctors,
+    changeAvailability
   };
   return (
     <AdminContext.Provider value={value}>
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -6,11 +6,12 @@ import {
 } from "../controllers/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
+import { changeAvailablity } from "../controllers/doctorController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 adminRouter.post("/all-doctors", authAdmin, allDoctors);
-
+adminRouter.post("/change-availability", authAdmin, changeAvailablity);
 export default adminRouter;
