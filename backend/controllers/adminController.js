dc67d6a5f5df0cb83230f7174dc8e14e9d0389be
manojLovesCoding <mkum9417@gmail.com>
2025-02-05 16:17:49 +0530

    CREATED API TO GET ALL DOCTOR LIST FOR ADMIN PANEL

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -103,4 +103,15 @@ const loginAdmin = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin };
+//API TO GET ALL DOCTOR LIST FOR ADMIN PANEL
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, loginAdmin, allDoctors };
