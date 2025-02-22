e6582eb0c60ed5a27e817026cd1a16fc419d9693
manojLovesCoding <mkum9417@gmail.com>
2025-02-22 10:39:37 +0530

    created api to cancel the appointment and Integrated it to the frontend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,8 @@ import {
   getProfile,
   updateProfile,
   bookAppointment,
-  listAppointment
+  listAppointment,
+  cancelAppointment
 } from "../controllers/userController.js";
 import authUser from "../middleware/authUser.js";
 import upload from "../middleware/multer.js";
@@ -22,5 +23,6 @@ userRouter.post(
 );
 userRouter.post("/book-appointment", authUser, bookAppointment);
 userRouter.get("/appointments", authUser, listAppointment);
+userRouter.post("/cancel-appointment", authUser, cancelAppointment);
 
 export default userRouter;
